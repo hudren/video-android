e63223d53b09c6724f18f90882377a88e9ac8d70
Jeff Hudren <hudren@gmail.com>
2015-06-13 08:38:31 -0600

    Fixed resource leak

--- video-client/src/main/java/com/hudren/homevideo/DownloadMonitor.java
+++ video-client/src/main/java/com/hudren/homevideo/DownloadMonitor.java
@@ -46,87 +46,97 @@ public class DownloadMonitor extends AsyncTask<Long, Void, Void>
             query.setFilterById( id );
 
             Cursor cursor = manager.query( query );
-            if ( cursor.moveToFirst() )
+            if ( cursor != null )
             {
-                int columnIndex = cursor.getColumnIndex( DownloadManager.COLUMN_STATUS );
-                int status = cursor.getInt( columnIndex );
-                int columnReason = cursor.getColumnIndex( DownloadManager.COLUMN_REASON );
-                int reason = cursor.getInt( columnReason );
-
-                switch ( status )
+                try
                 {
-                case DownloadManager.STATUS_FAILED:
-                    String failedReason = "";
-
-                    switch ( reason )
+                    if ( cursor.moveToFirst() )
                     {
-                    case DownloadManager.ERROR_CANNOT_RESUME:
-                        failedReason = "ERROR_CANNOT_RESUME";
-                        break;
-                    case DownloadManager.ERROR_DEVICE_NOT_FOUND:
-                        failedReason = "ERROR_DEVICE_NOT_FOUND";
-                        break;
-                    case DownloadManager.ERROR_FILE_ALREADY_EXISTS:
-                        failedReason = "ERROR_FILE_ALREADY_EXISTS";
-                        break;
-                    case DownloadManager.ERROR_FILE_ERROR:
-                        failedReason = "ERROR_FILE_ERROR";
-                        break;
-                    case DownloadManager.ERROR_HTTP_DATA_ERROR:
-                        failedReason = "ERROR_HTTP_DATA_ERROR";
-                        break;
-                    case DownloadManager.ERROR_INSUFFICIENT_SPACE:
-                        failedReason = "ERROR_INSUFFICIENT_SPACE";
-                        break;
-                    case DownloadManager.ERROR_TOO_MANY_REDIRECTS:
-                        failedReason = "ERROR_TOO_MANY_REDIRECTS";
-                        break;
-                    case DownloadManager.ERROR_UNHANDLED_HTTP_CODE:
-                        failedReason = "ERROR_UNHANDLED_HTTP_CODE";
-                        break;
-                    case DownloadManager.ERROR_UNKNOWN:
-                        failedReason = "ERROR_UNKNOWN";
-                        break;
+                        int columnIndex = cursor.getColumnIndex( DownloadManager.COLUMN_STATUS );
+                        int status = cursor.getInt( columnIndex );
+                        int columnReason = cursor.getColumnIndex( DownloadManager.COLUMN_REASON );
+                        int reason = cursor.getInt( columnReason );
+
+                        switch ( status )
+                        {
+                        case DownloadManager.STATUS_FAILED:
+                            String failedReason = "";
+
+                            switch ( reason )
+                            {
+                            case DownloadManager.ERROR_CANNOT_RESUME:
+                                failedReason = "ERROR_CANNOT_RESUME";
+                                break;
+                            case DownloadManager.ERROR_DEVICE_NOT_FOUND:
+                                failedReason = "ERROR_DEVICE_NOT_FOUND";
+                                break;
+                            case DownloadManager.ERROR_FILE_ALREADY_EXISTS:
+                                failedReason = "ERROR_FILE_ALREADY_EXISTS";
+                                break;
+                            case DownloadManager.ERROR_FILE_ERROR:
+                                failedReason = "ERROR_FILE_ERROR";
+                                break;
+                            case DownloadManager.ERROR_HTTP_DATA_ERROR:
+                                failedReason = "ERROR_HTTP_DATA_ERROR";
+                                break;
+                            case DownloadManager.ERROR_INSUFFICIENT_SPACE:
+                                failedReason = "ERROR_INSUFFICIENT_SPACE";
+                                break;
+                            case DownloadManager.ERROR_TOO_MANY_REDIRECTS:
+                                failedReason = "ERROR_TOO_MANY_REDIRECTS";
+                                break;
+                            case DownloadManager.ERROR_UNHANDLED_HTTP_CODE:
+                                failedReason = "ERROR_UNHANDLED_HTTP_CODE";
+                                break;
+                            case DownloadManager.ERROR_UNKNOWN:
+                                failedReason = "ERROR_UNKNOWN";
+                                break;
+                            }
+
+                            Log.w( TAG, "download failed: " + failedReason );
+                            done = true;
+                            break;
+
+                        case DownloadManager.STATUS_PAUSED:
+                            String pausedReason = "";
+
+                            switch ( reason )
+                            {
+                            case DownloadManager.PAUSED_QUEUED_FOR_WIFI:
+                                pausedReason = "PAUSED_QUEUED_FOR_WIFI";
+                                break;
+                            case DownloadManager.PAUSED_UNKNOWN:
+                                pausedReason = "PAUSED_UNKNOWN";
+                                break;
+                            case DownloadManager.PAUSED_WAITING_FOR_NETWORK:
+                                pausedReason = "PAUSED_WAITING_FOR_NETWORK";
+                                break;
+                            case DownloadManager.PAUSED_WAITING_TO_RETRY:
+                                pausedReason = "PAUSED_WAITING_TO_RETRY";
+                                break;
+                            }
+
+                            Log.d( TAG, "download paused: " + pausedReason );
+                            break;
+
+                        case DownloadManager.STATUS_PENDING:
+                            Log.d( TAG, "download pending" );
+                            break;
+
+                        case DownloadManager.STATUS_RUNNING:
+                            // Log.d( TAG, "download running" );
+                            break;
+
+                        case DownloadManager.STATUS_SUCCESSFUL:
+                            Log.i( TAG, "download successful" );
+                            done = true;
+                            break;
+                        }
                     }
-
-                    Log.w( TAG, "download failed: " + failedReason );
-                    done = true;
-                    break;
-
-                case DownloadManager.STATUS_PAUSED:
-                    String pausedReason = "";
-
-                    switch ( reason )
-                    {
-                    case DownloadManager.PAUSED_QUEUED_FOR_WIFI:
-                        pausedReason = "PAUSED_QUEUED_FOR_WIFI";
-                        break;
-                    case DownloadManager.PAUSED_UNKNOWN:
-                        pausedReason = "PAUSED_UNKNOWN";
-                        break;
-                    case DownloadManager.PAUSED_WAITING_FOR_NETWORK:
-                        pausedReason = "PAUSED_WAITING_FOR_NETWORK";
-                        break;
-                    case DownloadManager.PAUSED_WAITING_TO_RETRY:
-                        pausedReason = "PAUSED_WAITING_TO_RETRY";
-                        break;
-                    }
-
-                    Log.d( TAG, "download paused: " + pausedReason );
-                    break;
-
-                case DownloadManager.STATUS_PENDING:
-                    Log.d( TAG, "download pending" );
-                    break;
-
-                case DownloadManager.STATUS_RUNNING:
-                    // Log.d( TAG, "download running" );
-                    break;
-
-                case DownloadManager.STATUS_SUCCESSFUL:
-                    Log.i( TAG, "download successful" );
-                    done = true;
-                    break;
+                }
+                finally
+                {
+                    cursor.close();
                 }
             }
         }
